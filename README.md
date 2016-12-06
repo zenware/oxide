972f65ef86547527acfc7940dbc0f8c147abfa57
Jay Looney <jay.m.looney@gmail.com>
2016-12-06 01:36:02 -0600

    Get Daemonize working & choose two other libraries

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This is a project to build an experimental database in Rust.
 I hope it can take some influence from [project-m36.](https://github.com/agentm/project-m36)
+
+Rust Libs
+* [Metal IO](https://github.com/carllerche/mio)
+* [Nom](https://github.com/Geal/nom)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use] extern crate log;
+extern crate daemonize;
+
+use daemonize::{Daemonize};
+
 fn main() {
-    println!("Hello, world!");
-}
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/oxide.pid")
+        .chown_pid_file(true)
+        .working_directory("/tmp")
+        .user("nobody")
+        .group("daemon")
+        .umask(0o777)
+        .privileged_action(|| "Executed before drop privileges"); // Setup a listening socket.
+
+     match daemonize.start() {
+         Ok(_) => info!("Success, daemonized"),
+         Err(e) => error!("{}", e),
+     }
+ }
